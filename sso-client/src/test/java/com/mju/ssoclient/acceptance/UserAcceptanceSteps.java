032ecd728c889d73a93fcf4fafb2e8e7b2159856
Yunsik-Choi <choiys0311@gmail.com>
2023-04-20 17:10:37 +0900

    test : 회원가입 인수 테스트 작성

---
+++ sso-client/src/test/java/com/mju/ssoclient/acceptance/UserAcceptanceSteps.java
@@ -0,0 +1,39 @@
+package com.mju.ssoclient.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserAcceptanceSteps {
+    public static ExtractableResponse<Response> 회원가입_요청(
+            final String email,
+            final String password,
+            final String username,
+            final String nickname,
+            final String phoneNumber,
+            final String address,
+            final String gender,
+            final String birth
+    ) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+        params.put("username", username);
+        params.put("nickname", nickname);
+        params.put("phoneNumber", phoneNumber);
+        params.put("address", address);
+        params.put("gender", gender);
+        params.put("birth", birth);
+
+        return RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when()
+                .post("/user/join")
+                .then().log().all()
+                .extract();
+    }
+}
