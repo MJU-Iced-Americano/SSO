c5188e146f3452a785fd6c98891d59f971bd7368
Yunsik-Choi <choiys0311@gmail.com>
2023-04-20 17:09:49 +0900

    fix : KeycloakTest setUp 메서드 호출하도록 수정

--- sso-client/src/test/java/com/mju/ssoclient/acceptance/AcceptanceTest.java
+++ sso-client/src/test/java/com/mju/ssoclient/acceptance/AcceptanceTest.java
@@ -8,12 +8,13 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest extends KeycloakTest {
-
     @LocalServerPort
     private int port;
 
+    @Override
     @BeforeEach
     void setUp() {
+        super.setUp();
         RestAssured.port = port;
     }
 }
