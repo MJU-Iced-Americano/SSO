d1bad44101eae46af88a551f9ada7be869eb9ac7
Yunsik-Choi <choiys0311@gmail.com>
2023-04-22 02:02:47 +0900

    refactor : UserService 생성자 접근제어자 변경

--- sso-client/src/main/java/com/mju/ssoclient/application/UserService.java
+++ sso-client/src/main/java/com/mju/ssoclient/application/UserService.java
@@ -2,12 +2,13 @@ package com.mju.ssoclient.application;
 
 import com.mju.ssoclient.application.dto.UserJoinRequest;
 import com.mju.ssoclient.domain.UserRepository;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Service
 public class UserService {
     private final UserRepository userRepository;
