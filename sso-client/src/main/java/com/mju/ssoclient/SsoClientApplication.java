73cd69ba2afe8241ab4e6e4a74fc059c0fa85e52
Yunsik-Choi <choiys0311@gmail.com>
2023-04-22 02:00:12 +0900

    refactor : Main 클래스 접근 제어자 변경

--- sso-client/src/main/java/com/mju/ssoclient/SsoClientApplication.java
+++ sso-client/src/main/java/com/mju/ssoclient/SsoClientApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsoClientApplication {
+class SsoClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SsoClientApplication.class, args);
