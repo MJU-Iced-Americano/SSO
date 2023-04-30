419b3f36d60c2c56d424c14262418a225b66b05e
kimyubi <ieieie0419@gmail.co,=m>
2023-05-01 02:33:43 +0900

    Feat #15: keycloak & RDS 연동

---
+++ sso-client/src/main/java/com/mju/ssoclient/infrastructure/security/JwtAuthConverterProperties.java
@@ -0,0 +1,18 @@
+package com.mju.ssoclient.infrastructure.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Data;
+
+@Data
+@Validated
+@Configuration
+@ConfigurationProperties(prefix = "jwt.auth.converter")
+public class JwtAuthConverterProperties {
+
+	private String resourceId;
+	private String principalAttribute;
+
+}
