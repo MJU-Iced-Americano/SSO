27346a2dd506885b633a5549a28944fd7f24e8f2
Yunsik-Choi <choiys0311@gmail.com>
2023-05-29 08:12:54 +0900

    build : feign 의존성 제거

--- sso-client/src/main/java/com/mju/ssoclient/SsoClientApplication.java
+++ sso-client/src/main/java/com/mju/ssoclient/SsoClientApplication.java
@@ -2,10 +2,8 @@ package com.mju.ssoclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 class SsoClientApplication {
 
 	public static void main(String[] args) {
